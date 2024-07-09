4c2b0ddba48e0d05644daef38c61b6d1e6060f77
Ebru Akgun <ebru_akgun@yahoo.com>
2024-07-09 23:21:33 +0200

    final

--- js-6878.js
+++ js-6878.js
@@ -1,37 +1,52 @@
 
-function parisFunction(){
-  document.getElementById("button1-content").classList.remove("active-button")
-  document.getElementById("button3-content").classList.remove("active-button")
-  document.getElementById("button2-content").classList.add("active-button")
+// non dynamic version
+// function parisFunction(){
+//   document.getElementById("tab1-button").classList.remove("active-button")
+//   document.getElementById("tab3-button").classList.remove("active-button")
+//   document.getElementById("tab2-button").classList.add("active-button")
 
 
-  document.getElementById("tab1-content").classList.add("hidden")
-  document.getElementById("tab3-content").classList.add("hidden")
-  document.getElementById("tab2-content").classList.remove("hidden")
+//   document.getElementById("tab1-content").classList.add("hidden")
+//   document.getElementById("tab3-content").classList.add("hidden")
+//   document.getElementById("tab2-content").classList.remove("hidden")
+// }
+
+
+// function tokyoFunction(){
+//     document.getElementById("tab1-button").classList.remove("active-button")
+//     document.getElementById("tab2-button").classList.remove("active-button")
+//     document.getElementById("tab3-button").classList.add("active-button")
+  
+  
+//     document.getElementById("tab1-content").classList.add("hidden")
+//     document.getElementById("tab2-content").classList.add("hidden")
+//     document.getElementById("tab3-content").classList.remove("hidden")
+//   }
+
+//   function londonFunction(){
+//     document.getElementById("tab2-button").classList.remove("active-button")
+//     document.getElementById("tab3-button").classList.remove("active-button")
+//     document.getElementById("tab1-button").classList.add("active-button")
+  
+  
+//     document.getElementById("tab2-content").classList.add("hidden")
+//     document.getElementById("tab3-content").classList.add("hidden")
+//     document.getElementById("tab1-content").classList.remove("hidden")
+//   }
+  
+
+// dynamic version
+
+function dynamicFunction(activeTab){
+const buttonId=activeTab+"-button"
+const contentId=activeTab+"-content"
+for (element of document.getElementsByClassName("button-class")){
+    element.classList.remove("active-button")
 }
+document.getElementById(buttonId).classList.add("active-button")
 
-
-function tokyoFunction(){
-    document.getElementById("button1-content").classList.remove("active-button")
-    document.getElementById("button2-content").classList.remove("active-button")
-    document.getElementById("button3-content").classList.add("active-button")
-  
-  
-    document.getElementById("tab1-content").classList.add("hidden")
-    document.getElementById("tab2-content").classList.add("hidden")
-    document.getElementById("tab3-content").classList.remove("hidden")
-  }
-
-  function londonFunction(){
-    document.getElementById("button2-content").classList.remove("active-button")
-    document.getElementById("button3-content").classList.remove("active-button")
-    document.getElementById("button1-content").classList.add("active-button")
-  
-  
-    document.getElementById("tab2-content").classList.add("hidden")
-    document.getElementById("tab3-content").classList.add("hidden")
-    document.getElementById("tab1-content").classList.remove("hidden")
-  }
-  
-
-
+for (element of document.getElementsByClassName("content-class")){
+    element.classList.add("hidden")
+}
+document.getElementById(contentId).classList.remove("hidden")
+}
